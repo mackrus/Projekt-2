5d82bc646bce7ac9e3309537749d00a0a06cb2dd
kodmiller <erik.miller01@hotmail.com>
2025-05-19 15:39:17 +0200

    uppdaterade N till en numpy-array, lade till funktioner för felberäkning och logaritmisk skala i plotten för att förbättra visualiseringen av felet

--- solutions/d2.py
+++ solutions/d2.py
@@ -9,7 +9,7 @@ omega_0 = 3*np.pi
 gamma = 0.1
 # theta = theta' = 0
 T = 10
-N = [100,200,400,800]
+N = np.array([100,200,400,800])
 k = T/N[1]
 
 def theta(gamma,omega,omega_0,t):
@@ -34,12 +34,29 @@ felet = np.zeros(len(N))
 for i in range(len(N)):
    felet[i] = abs(S-S_trap[i])
 
+def fel(N):
+    S_trap = np.zeros(len(N))
+    diff = np.zeros(len(N))
+    for i in range(len(N)):
+        t = np.linspace(0,T,N[i])
+        S_trap[i] = trapezoid(speed(t),t)
+        diff[i] = abs(S_trap[i]-S)
+    return diff
+
+def q(fel, N):
+    # Skapa en ny lista med dubbla värden
+    N_double = [2*n for n in N]
+    return abs(np.log2(fel(N)/fel(N_double)))
 
 print(f'simpsons ger {S} error: {err}')
 print(f'felet i med stegen {N}: felet = {felet}')
 print(f'area under grafen {S_trap}')
 
-plt.plot(N,felet)
-plt.xlabel('Antal')
-plt.ylabel('felet')
+plt.plot(N,q(fel,N))
+plt.yscale('log')
+
+plt.xlabel('Antal $N$')
+plt.ylabel('felet med logskala')
+plt.savefig('errorplot')
+
 plt.show()
